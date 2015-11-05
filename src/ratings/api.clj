b0640af534ffd3d01576ab693e0929e6888025b2
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2015-11-05 16:13:35 +0100

    Created basic html template and routes

--- src/ratings/api.clj
+++ src/ratings/api.clj
@@ -1,17 +1,30 @@
 (ns ratings.api
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [clostache.parser :as tpl]))
+            [clostache.parser :as tpl]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 
 ;;Will be updated to call backend functions based on api calls. Needs a Post as well
 
-(defroutes ratings-api
- (route/resources "public")
- (GET "/players" {session :session
-                  headers :headers
-                  params  :query-params}
-      {:status 200
-       :body (tpl/render-resource "show-players.html")
-       :headers {"Content Type" "text/html"}}))
+(defroutes ratings-routes
+  ;(route/resources "public")  
+  (GET "/players" {session :session
+                   headers :headers
+                   params  :query-params}
+       {:status 200
+        :body (tpl/render-resource "players.html" {})
+        :headers {"Content-Type" "text/html"}})
+
+  (POST "/game" {session :session
+                 headers :headers
+                 params  :query-params}
+       {:status 200
+        :body (tpl/render-resource "players.html" {})
+        :headers {"Content-Type" "text/html"}})
   
+  (route/not-found "<h1>Page not found</h1>"))
+
+
+(def app
+  (wrap-defaults ratings-routes site-defaults))

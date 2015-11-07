69989e61bdd28f05f2ff734a78f1d3c174c325b8
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2015-11-07 02:44:40 +0100

    Added POST to REST. Just missing the posting in html for basic functionality

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
                  [compojure "1.4.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [http-kit "2.1.19"]]
   :plugins [[lein-ring "0.9.7"]]
--- src/ratings/api.clj
+++ src/ratings/api.clj
@@ -3,26 +3,41 @@
             [compojure.route :as route]
             [clostache.parser :as tpl]
             [ratings.glicko :as glicko]
+            [clojure.data.json :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-
-;;Will be updated to call backend functions based on api calls. Needs a Post as well
-
 (defroutes ratings-routes
-  ;(route/resources "public")  
-  (GET "/players" {session :session
+  (route/resources "public")  
+  (GET "/" {session :session
                    headers :headers
                    params  :query-params}
        {:status 200
         :body (tpl/render-resource "players.html" (glicko/get-players))
         :headers {"Content-Type" "text/html"}})
 
-  (POST "/game" {session :session
-                 headers :headers
-                 params  :query-params}
-       {:status 200
-        :body (tpl/render-resource "players.html" {})
-        :headers {"Content-Type" "text/html"}})
+  (POST "/add-game" {{:strs [white black result] :as params} :form-params session :session}
+         (try
+           (glicko/score-game white black result)
+           (catch com.fasterxml.jackson.core.JsonParseException e
+             (.printStackTrace e)
+             {:status 406
+              :headers {"Content-Type" "application/json"}
+              :body (json/write-str {:error (str "Unable to parse swagger endpoint.")})})
+           (catch IllegalArgumentException e
+             (.printStackTrace e)
+             {:status 406
+              :headers {"Content-Type" "application/json"}
+              :body (json/write-str {:error (.getMessage e)})})
+           (catch clojure.lang.ExceptionInfo e
+             (.printStackTrace e)
+             {:status 406
+              :headers {"Content-Type" "application/json"}
+              :body (json/write-str {:error (-> e ex-data :causes)})})
+           (catch Exception e
+             (.printStackTrace e)
+             {:status 500
+              :headers {"Content-Type" "application/json"}
+              :body (json/write-str {:error (str "An unexpected error occurred! ")})})))
   
   (route/not-found "<h1>Page not found</h1>"))
 

595f50e8ef8c076c2b0b0a5618e7b9fdac8990f5
quist <joakiml@gmail.com>
2015-12-02 01:17:58 +0100

    Introduced flux pattern for retrival of data. Lack actions

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,39 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
+var notify = require("gulp-notify");
 
-gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['./resources/public/js/app.js'],
+function buildScript(file, watch) {
+    var bundler = watchify(browserify({
+        entries: [file],
         transform: [reactify],
         debug: true,
         fullPaths: true // Requirement of watchify
+    }));
+
+    function rebundle() {
+        var stream = bundler.bundle();
+        return stream.on('error', notify.onError({
+            title: "Compile Error",
+            message: "<%= error.message %>"
+        }))
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./resources/public/build/'))
+        .pipe(notify("Rebundled.."));
+    }
+
+    bundler.on('update', function () {
+        rebundle();
+        console.log("Rebundling..");
     });
 
-    var watcher = watchify(bundler);
+    return rebundle();
+}
 
-    return watcher
-        .on('update', function() {
-            console.log("Updating..");
-            watcher.bundle()
-            .pipe(source('app.js'))
-            .pipe(gulp.dest('./resources/public/build/'));
-            console.log("Done..");
-        })
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./resources/public/build/'));
-        console.log("Done..");
-
-
+gulp.task('build', function() {
+    return buildScript('./resources/public/js/app.js', false);
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('default', ['build'], function() {
+    return buildScript('main.js', true);
+});
---
+++ resources/public/js/stores/PlayerStore.js
@@ -0,0 +1,60 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var PlayerConstants = require('../constants/PlayerConstants');
+var assign = require('object-assign');
+
+var CHANGE_EVENT = 'change';
+
+var _players = {list :[{name: "John"}]};
+
+
+var PlayerStore = assign({}, EventEmitter.prototype, {
+
+    getAll: function() {
+        return _players;
+    },
+
+
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    },
+
+    /**
+     * @param {function} callback
+     */
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    /**
+     * @param {function} callback
+     */
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+
+    dispatcherIndex: AppDispatcher.register(function(payload) {
+        var action = payload.action;
+        var text;
+
+        switch(action.actionType) {
+            case PlayerConstants.PLAYER_CREATE:
+                text = action.text.trim();
+                if (text !== '') {
+                    //create(text);
+                    PlayerStore.emitChange();
+                }
+                break;
+
+            case PlayerConstants.TODO_DESTROY:
+                //destroy(action.id);
+                PlayerStore.emitChange();
+                break;
+        }
+
+        return true; // No errors. Needed by promise in Dispatcher.
+    })
+
+});
+
+module.exports = PlayerStore;

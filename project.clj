718cb52be15cf26cfcd416c7cdc14c4cd1a56d69
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2015-12-03 16:44:22 +0100

    Fixed dependency issues in project.clj

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
                    :source-paths ["dev"]}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-defaults "0.1.5"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.novemberain/monger "3.0.0-rc2"]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
@@ -15,6 +14,7 @@
                  [org.clojure/data.json "0.2.5"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [http-kit "2.1.19"]
-                 [clj-time "0.11.0"]]
+                 [clj-time "0.11.0"]
+                 [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler ratings.api/app})

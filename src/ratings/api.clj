9051ce1705b515702807fa741398a7977dbba633
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2016-02-07 16:18:11 +0100

    Finished updating swagger setup. Fixes #23

--- src/ratings/api.clj
+++ src/ratings/api.clj
@@ -21,28 +21,29 @@
             :tags ["api"]
             (GET* "/leagues" []
                   :summary "Returns all leagues"
-                  :return [League]
+                  :return [league]
                  (ok (persistance/get-leagues)))
-            (GET* "/league" [id]
+            (GET* "/league" []
                   :summary "Return a specific league"
-                  :return League
-                  :form-params [id :- String]
+                  :return league
+                  :query-params [league-id :- String]
                   :description "Return a specific league. Requires BSON id as a form-param"
-                  (ok (persistance/get-league id)))
+                  (ok (persistance/get-league league-id)))
             (GET* "/:league/players" [league]
                   :summary "Return all players"
                   :path-params [league :- String]
                   :return [player]
-                  (ok (persistance/get-leagues league)))
+                  (ok (persistance/get-players league)))
             (GET* "/:league/games" [league]
                   :summary "Return all games"
                   :path-params [league :- String]
-                  :return [games]
+                  :return [game]
                   (ok (persistance/get-games league)))
             (POST* "/:league/games" [league]
                    :summary "Add a game to the system"
                    :path-params [league :- String]
                    :form-params [whiteId :- String blackId :- String result :- String]
+                   :return game
                    (ok (persistance/score-game whiteId blackId result league)))
             (POST* "/:league/player" [league]
                    :summary "Add a player to the system"
@@ -53,17 +54,15 @@
             (POST* "/leagues" []
                    :summary "Add a league to the system"
                    :form-params [league-name :- String settings :- s/Any]
-                   :return League
+                   :return league
                    (ok (persistance/create-league league-name settings)))
             (DELETE* "/:league/player" [league]
                      :summary "Delete a player from the system"
                      :form-params [player-id :- String]
-                     :return player
                      (ok (persistance/delete-player player-id league)))
             (DELETE* "/:league/game" [league]
                      :summary "Delete a game from the stystem"
                      :form-params [game-id :- String]
-                     :return game
                      (ok (persistance/delete-game game-id league)))))
 
 (def app
--- src/ratings/schemas.clj
+++ src/ratings/schemas.clj
@@ -2,20 +2,21 @@
   (:require [schema.core :as s])
   (:import [org.bson.types ObjectId]))
 
+
 (s/defschema player
   {:name s/Str
    :rating s/Num
    :rating-rd s/Num
    :volatility s/Num
-   :_id s/Str})
+   :_id s/Any})
 
-(s/defschema League
+(s/defschema league
   {:name s/Str
-   :_id s/Str})
+   :_id s/Any})
 
-(s/defschema games
-  {:white-id s/Str
-   :black-id s/Num
-   :result s/Num
-   :score s/Num
-   :_id s/Str})
+(s/defschema game
+  {:white s/Str
+   :black s/Str
+   :result s/Str
+   :added s/Str
+   :_id s/Any})
